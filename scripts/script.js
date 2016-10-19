08c1a9e811435ac42ca7b50a0c9fb9d2976933d4
cclifto <cliftonconner@gmail.com>
2016-10-19 15:05:26 -0500

    added search function

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,7 @@
 var houseColumn = document.querySelector('.house-column'),
 	senateColumn = document.querySelector('.senate-column')
 	inputNode = document.querySelector('input'),
-	baseUrl = "http://congress.api.sunlightfoundation.com/bills?apikey=149a2a8730aa4936bab76017c47d8dab"
+	baseUrl = "http://congress.api.sunlightfoundation.com/bills/search?apikey=149a2a8730aa4936bab76017c47d8dab"
 
 var billsToHTML = function(billsArray){
 	var htmlString = ''
@@ -19,6 +19,8 @@ var billsToHTML = function(billsArray){
 		htmlString += '<h2>' + (billDetails.short_title ? billDetails.short_title : billDetails.bill_id) + '</h2>'
 		htmlString += '<h3>' + billDetails.official_title + '</h3>'
 		htmlString += '<h5>' + billDetails.sponsor.first_name + " " + billDetails.sponsor.last_name + '</h5>'
+		htmlString += '<h6>Introduced On: ' + billDetails.introduced_on + '</h6>'
+		htmlString += '<h6>Last Action At: ' + billDetails.last_action_at + '</h6>'
 		htmlString += '</div>'
 		//we should build the full htmlString and then write to innerHTML
 		//only once
@@ -40,17 +42,29 @@ var senateResponseHandler = function(apiResponse){
 	senateColumn.innerHTML = htmlString
 }
 
-var fetchSenateBills = function() {
-	var url = baseUrl + '&chamber=senate'
+var fetchSenateBills = function(searchQuery) {
+	var url = baseUrl + '&chamber=senate&query=' + searchQuery
 	var promise = $.getJSON(url)
 	promise.then(senateResponseHandler)
 }
 
-var fetchHouseBills = function() {
-	var url = baseUrl + '&chamber=house'
+var fetchHouseBills = function(searchQuery) {
+	var url = baseUrl + '&chamber=house&query=' + searchQuery
 	var promise = $.getJSON(url)
 	promise.then(houseResponseHandler)
 }
+
+var search = function(event){
+	if(event.keyCode === 13){
+		var searchQuery = event.target.value
+		fetchHouseBills(searchQuery)
+		fetchSenateBills(searchQuery)
+		event.target.value = ""
+	}
+
+}
+
+inputNode.addEventListener('keydown', search)
 
 fetchHouseBills()
 fetchSenateBills()
